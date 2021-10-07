2fce880bd8676356201b46a7b4428eb6b971b173
GuckTube YT <56192597+GuckTubeYT@users.noreply.github.com>
2021-10-07 19:41:53 +0700

    Version 14 Items dat (Fix Items are unordered)

--- enet server test/enet server test.cpp
+++ enet server test/enet server test.cpp
@@ -1427,6 +1427,7 @@ void buildItemsDatabase()
 		}
 		if (itemsdatVersion >= 12) memPos += 13;
 		if (itemsdatVersion >= 13) memPos += 4;
+		if (itemsdatVersion >= 14) memPos += 4;
 		if (i != tile.id)
 			cout << "Item are unordered!" << i << "/" << tile.id << endl;
 
